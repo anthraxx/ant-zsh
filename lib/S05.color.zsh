260d506282014fdd23573bad5a4772549c018443
anthraxx <levente@leventepolyak.net>
2018-08-03 17:22:56 +0200

    style: replace whitespace with tabs

--- lib/S05.color.zsh
+++ lib/S05.color.zsh
@@ -10,26 +10,26 @@ fi
 # enable ls colors
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
-  # Find the option for using colors in ls, depending on the version: Linux or BSD
-  ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+	# Find the option for using colors in ls, depending on the version: Linux or BSD
+	ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
 fi
 
 # enable ip colors
 if [ "$DISABLE_IP_COLORS" != "true" ]
 then
-  ip --color -br link &>/dev/null 2>&1 && alias ip='ip --color -br'
+	ip --color -br link &>/dev/null 2>&1 && alias ip='ip --color -br'
 fi
 
 # enable diff colors
 if [ "$DISABLE_DIFF_COLORS" != "true" ]
 then
-  diff --color=auto --version &>/dev/null 2>&1 && alias diff='diff --color=auto'
+	diff --color=auto --version &>/dev/null 2>&1 && alias diff='diff --color=auto'
 fi
 
 # enable ncdu colors
 if [ "$DISABLE_NCDU_COLORS" != "true" ]
 then
-  ncdu --color=dark -version &>/dev/null 2>&1 && alias ncdu='ncdu --color=dark'
+	ncdu --color=dark -version &>/dev/null 2>&1 && alias ncdu='ncdu --color=dark'
 fi
 
 if [ "$DISABLE_S_COLORS" != "true" ]
@@ -40,7 +40,7 @@ fi
 # set some colors
 for COLOR in RED GREEN BLUE YELLOW WHITE BLACK CYAN; do
 	eval PR_$COLOR='%{$fg[${(L)COLOR}]%}'
-    eval PR_BRIGHT_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
+	eval PR_BRIGHT_$COLOR='%{$fg_bold[${(L)COLOR}]%}'
 done
 PR_RST="%{${reset_color}%}"
 PR_RESET="%{%b%s%u$reset_color%}"
